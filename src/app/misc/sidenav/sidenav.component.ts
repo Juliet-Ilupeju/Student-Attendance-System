d82233ed6962fc59b648d1806cad287798f0c26f
Juliet-Ilupeju <38299861+Juliet-Ilupeju@users.noreply.github.com>
2020-08-03 14:38:09 +0000

    updates on logout function

--- src/app/misc/sidenav/sidenav.component.ts
+++ src/app/misc/sidenav/sidenav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-sidenav',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidenavComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit(): void {
   }
 
+  public logoutUser() {
+    this.authService.logout();
+  }
 }
